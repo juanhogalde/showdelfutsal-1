7dbb723c4ee3cc255de6ce1935eec151f13fd2cc
IgnacioLowa <ignacio@lowa.com.ar>
2021-09-28 12:40:29 -0500

    Home de prueba

--- frontend/src/Componentes/Home/Home.js
+++ frontend/src/Componentes/Home/Home.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import RutasPrivadas from '../../Rutas/RutasPrivadas';
 import RutasPublicas from '../../Rutas/RutasPublicas';
+import Inicio from '../Inicio/Inicio';
 
 const Home = () => {
-  const {logueado} = useSelector(state => state.storePrueba);
-  return <div>{logueado ? <RutasPrivadas /> : <RutasPublicas />}</div>;
+  // const {logueado} = useSelector(state => state.storePrueba);
+  // return <div>{logueado ? <RutasPrivadas /> : <RutasPublicas />}</div>;
+  return (
+    <React.Fragment>
+      <Inicio> </Inicio>
+    </React.Fragment>
+  );
 };
 
 export default Home;
