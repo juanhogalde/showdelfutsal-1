09d0ad701ead63ce7dab32c67f54569c50732019
IgnacioLowa <ignacio@lowa.com.ar>
2021-09-29 11:56:07 -0500

    Componente Pagina Somos

---
+++ frontend/src/Componentes/PaginaSomos/PaginaSomos.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './PaginaSomos.css';
+
+const PaginaSomos = () => {
+  return (
+    <div>
+      <div className="LI-Nosotros ">
+        {/* <div className="CP-Somos">
+          <div className="CI-Somos"></div>
+          <div className="CI-Somos"></div>
+          <div className="CI-Somos"></div>
+          <div className="CI-Somos"></div>
+        </div> */}
+        s
+      </div>
+    </div>
+  );
+};
+
+export default PaginaSomos;
