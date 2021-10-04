cf6589e8ccd27ac4da59b9bcbadaa8e7a76ce5fe
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-04 11:46:54 -0500

    correcciones grid Inicio sección Marcador

--- frontend/src/Componentes/Inicio/Inicio.js
+++ frontend/src/Componentes/Inicio/Inicio.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import BarraDeNavegacion from '../BarraDeNavegacion/BarraDeNavegacion';
+import Marcador from '../Marcador/Marcador';
 import PieDepagina from '../PieDePagina/PieDepagina';
+import publicidadCorta from '../../Static/Img/publicidad_corta.jpg';
 import './Inicio.css';
 
 const Inicio = () => {
@@ -8,31 +10,45 @@ const Inicio = () => {
     <div className="LP-Inicio">
       {/* <BarraDeNavegacion /> */}
       {/* SECCION BANNER INICIO */}
-      <div className="LI-Inicio seccion-somos">
+      {/* <div className="LI-Inicio seccion-somos">
         <div className="CP-Somos">
           <div className="CI-Somos"></div>
           <div className="CI-Somos"></div>
           <div className="CI-Somos"></div>
           <div className="CI-Somos"></div>
         </div>
-      </div>
+      </div> */}
       {/* SECCION VIVO */}
-      <div className="LI-Inicio seccion-vivo Margen-Vivo">
+      {/* <div className="LI-Inicio seccion-vivo Margen-Vivo">
         <div className="CP-Vivo">
           <div className="CI-Vivo"></div>
           <div className="CI-Vivo"></div>
           <div className="CI-Vivo"></div>
         </div>
-      </div>
+      </div> */}
       {/* SECCION MARCADOR */}
       <div className="LI-Inicio seccion-marcador">
         <div className="CP-Marcador">
-          <div className="CI-Marcador"></div>
-          <div className="CI-Marcador"></div>
+          <div className="CI-Marcador">
+            <div className="titulo-Marcador">
+              <p style={{fontSize: '80px'}}>PARTIDOS</p>
+            </div>
+            <div className="componente-Marcador">
+              <Marcador />
+            </div>
+          </div>
+          <div className="CI-Publicidad-Marcador">
+            <div>
+              <img alt="" src={publicidadCorta}></img>
+            </div>
+            <div>
+              <img alt="" src={publicidadCorta}></img>
+            </div>
+          </div>
         </div>
       </div>
       {/* SECCION NOTICIAS */}
-      <div className="LI-Inicio seccion-noticias  Margen-Noticias">
+      {/* <div className="LI-Inicio seccion-noticias  Margen-Noticias">
         <div className="CP-Noticias">
           <div className="CI-Noticias"></div>
           <div className="CI-Noticias"></div>
@@ -42,14 +58,14 @@ const Inicio = () => {
           </div>
           <div className="CI-Noticias"></div>
         </div>
-      </div>
+      </div> */}
       {/* SECCION GALERIA */}
-      <div className="LI-Inicio seccion-galeria">
+      {/* <div className="LI-Inicio seccion-galeria">
         <div className="CP-Galeria">
           <div className="CI-Galeria"></div>
           <div className="CI-Galeria"></div>
         </div>
-      </div>
+      </div> */}
       {/* <PieDepagina /> */}
     </div>
   );
