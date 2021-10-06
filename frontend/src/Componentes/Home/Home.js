108b87690ef97f94c74d3036fae7777206c595f1
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-06 08:19:20 -0500

    componente inicio

--- frontend/src/Componentes/Home/Home.js
+++ frontend/src/Componentes/Home/Home.js
@@ -4,8 +4,13 @@ import RutasPrivadas from '../../Rutas/RutasPrivadas';
 import RutasPublicas from '../../Rutas/RutasPublicas';
 import BarraDeNavegacion from '../BarraDeNavegacion/BarraDeNavegacion';
 import Inicio from '../Inicio/Inicio';
+import NoticiasMiniatura from '../NoticiasMiniatura/NoticiasMiniatura';
 import PaginaSomos from '../PaginaSomos/PaginaSomos';
-
+const Filtro = [
+  {nombre: 'Femenino', link: '/link'},
+  {nombre: 'Masculino', link: '/link'},
+  {nombre: 'Inferiores', link: '/NoticiaDesarrollada'},
+];
 const Home = () => {
   // const {logueado} = useSelector(state => state.storePrueba);
   // return <div>{logueado ? <RutasPrivadas /> : <RutasPublicas />}</div>;
