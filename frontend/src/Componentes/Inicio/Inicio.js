02f797babcbe4bdffa2f63dbd1265a8c5d7c5bd3
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-06 08:19:05 -0500

    trabajando con componente Inicio

--- frontend/src/Componentes/Inicio/Inicio.js
+++ frontend/src/Componentes/Inicio/Inicio.js
@@ -4,8 +4,20 @@ import Marcador from '../Marcador/Marcador';
 import PieDepagina from '../PieDePagina/PieDepagina';
 import publicidadCorta from '../../Static/Img/publicidad_corta.jpg';
 import './Inicio.css';
+import Filtros from '../Filtros/Filtros';
+import NoticiasMiniatura from '../NoticiasMiniatura/NoticiasMiniatura';
+import storePrueba from '../../Redux/Prueba/ReducerPrueba';
+import {useSelector} from 'react-redux';
+import publicidadLarga from '../../Static/Img/publicidad_larga.jpg';
+const Filtro = [
+  {nombre: 'Femenino', link: '/link'},
+  {nombre: 'Masculino', link: '/link'},
+  {nombre: 'Inferiores', link: '/NoticiaDesarrollada'},
+];
 
 const Inicio = () => {
+  const noticia = useSelector(state => state.storePrueba.noticias);
+  console.log(noticia);
   return (
     <div className="LP-Inicio">
       {/* <BarraDeNavegacion /> */}
@@ -27,7 +39,7 @@ const Inicio = () => {
         </div>
       </div> */}
       {/* SECCION MARCADOR */}
-      <div className="LI-Inicio seccion-marcador">
+      {/* <div className="LI-Inicio seccion-marcador">
         <div className="CP-Marcador">
           <div className="CI-Marcador">
             <div className="titulo-Marcador">
@@ -46,19 +58,38 @@ const Inicio = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
       {/* SECCION NOTICIAS */}
-      {/* <div className="LI-Inicio seccion-noticias  Margen-Noticias">
+      <div className="LI-Inicio seccion-noticias  Margen-Noticias">
         <div className="CP-Noticias">
-          <div className="CI-Noticias"></div>
+          <div className="CI-Filtros">
+            <Filtros filtros={Filtro}></Filtros>
+          </div>
+          <div className="CI-NoticiaPrincipal">
+            <div className="componenteNoticiaPrincipal">
+              <NoticiasMiniatura isSobreImagen={true}></NoticiasMiniatura>
+            </div>
+          </div>
+          <div className="CI-NoticiasMini">
+            <div className="noticia-Miniatura-1">
+              <NoticiasMiniatura datosModelado={noticia}></NoticiasMiniatura>
+            </div>
+            <div className="noticia-Miniatura-2">
+              <NoticiasMiniatura datosModelado={noticia}></NoticiasMiniatura>
+            </div>
+          </div>
+          <div className="publicidad-Noticias">
+            <img alt="" src={publicidadLarga}></img>
+          </div>
+          {/* <div className="CI-Noticias"></div>
           <div className="CI-Noticias"></div>
           <div className="Noticias-Miniaturas">
             <div className="Noticias"></div>
             <div className="Noticias"></div>
           </div>
-          <div className="CI-Noticias"></div>
+          <div className="CI-Noticias"></div> */}
         </div>
-      </div> */}
+      </div>
       {/* SECCION GALERIA */}
       {/* <div className="LI-Inicio seccion-galeria">
         <div className="CP-Galeria">
