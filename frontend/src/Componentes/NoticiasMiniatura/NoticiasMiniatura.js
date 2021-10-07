6ab969bde1043036c9d31e3bc6f05b3961abcc1a
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-07 13:11:28 -0500

    correcciones NoticiasMiniatura

--- frontend/src/Componentes/NoticiasMiniatura/NoticiasMiniatura.js
+++ frontend/src/Componentes/NoticiasMiniatura/NoticiasMiniatura.js
@@ -19,68 +19,70 @@ const NoticiasMiniatura = ({
   console.log(isSeccionNoticias);
   return (
     <div className="CP-Noticias-NoticiaMiniatura">
-      {isSeccionNoticias && (
-        <div className="CI-Cabecera-NoticiaMiniatura">
-          {isSobreImagen && (
-            <div className="CI-Categoria-NoticiaMiniatura-Principal">
+      <div className="cuerpo-NoticiaMiniatura">
+        {isSeccionNoticias && (
+          <div className="CI-Cabecera-NoticiaMiniatura">
+            {isSobreImagen && (
+              <div className="CI-Categoria-NoticiaMiniatura-Principal">
+                <EtiquetaCategoria
+                  categoriaId={datosModelado.categoria ? datosModelado.categoria : 0}
+                />
+              </div>
+            )}
+            <div className="CI-SubCategoria-Fecha-NoticiaMiniatura">
               <EtiquetaCategoria
-                categoriaId={datosModelado.categoria ? datosModelado.categoria : 0}
+                subcategoria={true}
+                categoriaId={datosModelado.subcategoria ? datosModelado.subcategoria : 0}
               />
+              <p className="fecha-NoticiaMiniatura-NoticiaMiniatura">
+                {datosModelado.fecha ? datosModelado.fecha : 'dd/mm/aaaa'}
+              </p>
             </div>
-          )}
-
-          <div className="CI-SubCategoria-Fecha-NoticiaMiniatura">
-            <EtiquetaCategoria
-              subcategoria={true}
-              categoriaId={datosModelado.subcategoria ? datosModelado.subcategoria : 0}
-            />
-            <p className="fecha-NoticiaMiniatura-NoticiaMiniatura">
-              {datosModelado.fecha ? datosModelado.fecha : 'dd/mm/aaaa'}
-            </p>
           </div>
-        </div>
-      )}
+        )}
 
-      <div className="CI-Cuerpo-NoticiaMiniatura">
-        <div
-          className={`${
-            isSobreImagen
-              ? 'contenedor-Imagen-NoticiaMiniatura-Principal'
-              : 'contenedor-Imagen-NoticiaMiniatura'
-          }`}
-        >
-          <img
+        <div className="CI-Cuerpo-NoticiaMiniatura">
+          <div
             className={`${
-              isSobreImagen ? 'imagenNoticia-Miniatura-Principal' : 'imagenNoticia-Miniatura'
+              isSobreImagen
+                ? 'contenedor-Imagen-NoticiaMiniatura-Principal'
+                : 'contenedor-Imagen-NoticiaMiniatura'
             }`}
-            src={datosModelado.fuenteImg ? datosModelado.fuenteImg : imagenNoticia}
-            alt="imagen"
-          />
-          {!isSobreImagen && (
-            <div className="CI-Categoria-NoticiaMiniatura">
-              <EtiquetaCategoria
-                categoriaId={datosModelado.categoria ? datosModelado.categoria : 0}
-              />
-            </div>
-          )}
-        </div>
-        <div>
-          <p
-            className={
-              isSobreImagen ? 'sobreImagen-NoticiaMiniatura m-0' : 'm-0 titulo-NoticiaMiniatura'
-            }
           >
-            {datosModelado.titulo
-              ? `${
-                  isSobreImagen ? datosModelado.titulo : datosModelado.titulo.substring(0, 100)
-                } ...`
-              : 'Sin titulo largo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'}
-          </p>
-        </div>
+            <img
+              className={`${
+                isSobreImagen ? 'imagenNoticia-Miniatura-Principal' : 'imagenNoticia-Miniatura'
+              }`}
+              src={datosModelado.fuenteImg ? datosModelado.fuenteImg : imagenNoticia}
+              alt="imagen"
+            />
+            {!isSobreImagen && (
+              <div className="CI-Categoria-NoticiaMiniatura">
+                <EtiquetaCategoria
+                  categoriaId={datosModelado.categoria ? datosModelado.categoria : 0}
+                />
+              </div>
+            )}
+            {isSobreImagen && (
+              <p className={'sobreImagen-NoticiaMiniatura m-0'}>
+                {datosModelado.titulo
+                  ? `${datosModelado.titulo.substring(0, 100)} ...`
+                  : 'Sin titulo largo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'}
+              </p>
+            )}
+          </div>
+          {!isSobreImagen && (
+            <p className={'m-0 titulo-NoticiaMiniatura'}>
+              {datosModelado.titulo
+                ? `${datosModelado.titulo.substring(0, 90)}...`
+                : 'Sin titulo largo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'}
+            </p>
+          )}
 
-        {/* <h4 className="Copete-NoticiaMiniatura">
-          {datosModelado.copete ? datosModelado.copete : 'sin copete'}
-        </h4> */}
+          {/* <h4 className="Copete-NoticiaMiniatura">
+            {datosModelado.copete ? datosModelado.copete : 'sin copete'}
+          </h4> */}
+        </div>
       </div>
     </div>
   );
