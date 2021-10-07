c17054d2283c0c81bfd73f346785a8189b8f27df
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-07 12:37:31 -0500

    correcciones Noticia Miniatura

--- frontend/src/Componentes/NoticiasMiniatura/NoticiasMiniatura.js
+++ frontend/src/Componentes/NoticiasMiniatura/NoticiasMiniatura.js
@@ -11,29 +11,73 @@ import imagenNoticia from '../../Static/Img/noticia.jfif';
  */
 //TODO: luego de probar quitar datosDePrueba
 
-const NoticiasMiniatura = ({datosModelado = {}, isSobreImagen = false}) => {
+const NoticiasMiniatura = ({
+  datosModelado = {},
+  isSobreImagen = false,
+  isSeccionNoticias = false,
+}) => {
+  console.log(isSeccionNoticias);
   return (
     <div className="CP-Noticias-NoticiaMiniatura">
-      {/* <div className="CI-Cabecera-NoticiaMiniatura">
-        <div className="CI-Categoria-NoticiaMiniatura">
-          <EtiquetaCategoria categoriaId={datosModelado.categoria ? datosModelado.categoria : 0} />
+      {isSeccionNoticias && (
+        <div className="CI-Cabecera-NoticiaMiniatura">
+          {isSobreImagen && (
+            <div className="CI-Categoria-NoticiaMiniatura-Principal">
+              <EtiquetaCategoria
+                categoriaId={datosModelado.categoria ? datosModelado.categoria : 0}
+              />
+            </div>
+          )}
+
+          <div className="CI-SubCategoria-Fecha-NoticiaMiniatura">
+            <EtiquetaCategoria
+              subcategoria={true}
+              categoriaId={datosModelado.subcategoria ? datosModelado.subcategoria : 0}
+            />
+            <p className="fecha-NoticiaMiniatura-NoticiaMiniatura">
+              {datosModelado.fecha ? datosModelado.fecha : 'dd/mm/aaaa'}
+            </p>
+          </div>
         </div>
-        <div className="CI-SubCategoria-Fecha-NoticiaMiniatura">
-          <EtiquetaCategoria
-            subcategoria={true}
-            categoriaId={datosModelado.subcategoria ? datosModelado.subcategoria : 0}
+      )}
+
+      <div className="CI-Cuerpo-NoticiaMiniatura">
+        <div
+          className={`${
+            isSobreImagen
+              ? 'contenedor-Imagen-NoticiaMiniatura-Principal'
+              : 'contenedor-Imagen-NoticiaMiniatura'
+          }`}
+        >
+          <img
+            className={`${
+              isSobreImagen ? 'imagenNoticia-Miniatura-Principal' : 'imagenNoticia-Miniatura'
+            }`}
+            src={datosModelado.fuenteImg ? datosModelado.fuenteImg : imagenNoticia}
+            alt="imagen"
           />
-          <p className="fecha-NoticiaMiniatura-NoticiaMiniatura">
-            {datosModelado.fecha ? datosModelado.fecha : 'dd/mm/aaaa'}
+          {!isSobreImagen && (
+            <div className="CI-Categoria-NoticiaMiniatura">
+              <EtiquetaCategoria
+                categoriaId={datosModelado.categoria ? datosModelado.categoria : 0}
+              />
+            </div>
+          )}
+        </div>
+        <div>
+          <p
+            className={
+              isSobreImagen ? 'sobreImagen-NoticiaMiniatura m-0' : 'm-0 titulo-NoticiaMiniatura'
+            }
+          >
+            {datosModelado.titulo
+              ? `${
+                  isSobreImagen ? datosModelado.titulo : datosModelado.titulo.substring(0, 100)
+                } ...`
+              : 'Sin titulo largo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'}
           </p>
         </div>
-      </div> */}
-      <div className="CI-Cuerpo-NoticiaMiniatura">
-        <img src={datosModelado.fuenteImg ? datosModelado.fuenteImg : imagenNoticia} alt="imagen" />
 
-        <p className={isSobreImagen ? 'sobreImagen-NoticiaMiniatura' : ''}>
-          {datosModelado.titulo ? datosModelado.titulo : 'Sin titulo'}
-        </p>
         {/* <h4 className="Copete-NoticiaMiniatura">
           {datosModelado.copete ? datosModelado.copete : 'sin copete'}
         </h4> */}
