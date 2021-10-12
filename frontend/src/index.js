246fb96189a7cb4c9cc8ed8dd9da98fa963fbb9d
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-12 10:57:25 -0300

    Comente boostrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux';
 import store from './Redux/Store';
 import Home from './Componentes/Home/Home';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 /* import 'bootstrap/dist/css/bootstrap.min.css'; */
 ReactDOM.render(
