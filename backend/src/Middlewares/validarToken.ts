8057458d7c9c5873d88f8f6715a980e07dbc6e45
LeoLowa <leo@lowa.com.ar>
2021-10-14 00:22:57 -0300

    Validacion token

--- backend/src/Middlewares/validarToken.ts
+++ backend/src/Middlewares/validarToken.ts
@@ -2,7 +2,7 @@ import * as jwt from 'jsonwebtoken';
 import {Request, Response, NextFunction} from 'express';
 import responder from '../Middlewares/responder';
 
-export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+export const validarToken = (req: Request, res: Response, next: NextFunction) => {
   const token = <string>req.headers['auth'];
   let jwtPayload;
   let clavePrivada = process.env.TOKEN_SECRET ? process.env.TOKEN_SECRET : '';
