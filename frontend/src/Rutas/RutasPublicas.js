3336f0200344ae684433fc34b102f50d65743ae1
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-15 11:54:43 -0300

    Ruta Seccion noticias

--- frontend/src/Rutas/RutasPublicas.js
+++ frontend/src/Rutas/RutasPublicas.js
@@ -3,12 +3,15 @@ import {Route} from 'react-router';
 import {Switch} from 'react-router-dom';
 import PaginaSomos from '../Componentes/PaginaSomos/PaginaSomos';
 import Inicio from '../Componentes/Inicio/Inicio';
+
+import SeccionNoticias from '../Componentes/SeccionNoticias/SeccionNoticias';
 const RutasPublicas = () => {
   return (
     <React.Fragment>
       <Switch>
         <Route exact component={Inicio} path="/"></Route>
         <Route component={PaginaSomos} path="/Somos"></Route>
+        <Route component={SeccionNoticias} path="/Noticias"></Route>
       </Switch>
     </React.Fragment>
   );
