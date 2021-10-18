e413edaeabcfd860e24357d10020771b85b413cd
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-18 09:02:43 -0500

    index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './Redux/Store';
-/* import Home from './Componentes/Home/Home'; */
+import Home from './Componentes/Home/Home';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-/* import GaleriaNoticiasMiniatura from './Componentes/SliderNoticias/SliderNoticias'; */
-import SliderNoticias from './Componentes/SliderNoticias/SliderNoticias';
-/* import 'bootstrap/dist/css/bootstrap.min.css'; */
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      {/* <Home></Home> */}
-      <SliderNoticias />
+      <Home></Home>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
