463fbb6be8cee36026a6014e24166994c48b4bc5
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-18 14:56:38 -0300

    SubMenuNoticias NavLink

--- frontend/src/Componentes/SubMenuNoticias/SubMenuNoticias.js
+++ frontend/src/Componentes/SubMenuNoticias/SubMenuNoticias.js
@@ -1,50 +1,58 @@
 import React from 'react';
 import './SubMenuNoticias.css';
+
+import {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 
 const SubMenuNoticias = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="CP-SN-Noticias-Menu">
       <div className="CI-Noticias-titulo">
         <h4>CATEGORIA SECCION</h4>
       </div>
       <div className="CI-Noticias-Menu">
-        {/* <ul>
+        <ul
+          className={
+            click ? 'I-Contenedor-Submenu-Noticias active' : 'I-Contenedor-Submenu-Noticias'
+          }
+        >
           <li className="I-Noticias-fixture">
             <NavLink
               exact
-              to="/Noticias"
+              to="/"
               activeClassName="active"
-              className="nav-links"
-              //   onClick={handleClick}
+              className="SubMenu-Noticias-Links"
+              onClick={handleClick}
             >
-              Fixture
+              <h6>FIXTURE</h6>
             </NavLink>
           </li>
           <li className="I-Noticias-tabla-posiciones">
             <NavLink
               exact
-              to="/Noticias"
+              to=""
               activeClassName="active"
-              className="nav-links"
-              //   onClick={handleClick}
+              className="SubMenu-Noticias-Links"
+              onClick={handleClick}
             >
-              Fixture
+              <h6>TABLA DE POSICIONES </h6>
             </NavLink>
           </li>
           <li className="I-Noticias-categoria">
             <NavLink
               exact
-              to="/Noticias"
+              to=""
               activeClassName="active"
-              className="nav-links"
-              //   onClick={handleClick}
+              className="SubMenu-Noticias-Links"
+              onClick={handleClick}
             >
-              Fixture
+              <h6>NOTICIAS </h6>
             </NavLink>
           </li>
-        </ul> */}
-        <div className="I-Noticias-fixture">
+        </ul>
+        {/* <div className="I-Noticias-fixture">
           <h6>Fixture</h6>
         </div>
         <div className="I-Noticias-tabla-posiciones">
@@ -52,7 +60,7 @@ const SubMenuNoticias = () => {
         </div>
         <div className="I-Noticias-categoria">
           <h6>Noticias</h6>
-        </div>
+        </div> */}
       </div>
       <div className="I-Barra"></div>
     </div>
