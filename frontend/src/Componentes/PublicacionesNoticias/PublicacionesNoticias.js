57d6bfc388471a0bcab6ddc217760bf7369330c0
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-19 15:50:07 -0300

    Componente Fechas de publicaciones noticias

---
+++ frontend/src/Componentes/PublicacionesNoticias/PublicacionesNoticias.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PublicacionesNoticias.css';
+
+const PublicacionesNoticias = () => {
+  return (
+    <div className="CP-Publicaciones-noticias">
+      <div className="CI-Panel-publicaciones">
+        <div className="I-Cabecera-publicaciones"></div>
+
+        <div className="I-Fechas-publicaciones"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PublicacionesNoticias;
