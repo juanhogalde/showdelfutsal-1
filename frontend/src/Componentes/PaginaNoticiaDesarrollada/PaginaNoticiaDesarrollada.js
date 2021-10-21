82b84ccb92f32611b37c3b8b95ac70f18dbd735b
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-21 12:31:10 -0300

    feat: MAQUETACION: de una noticia (arregloe mediaquerie )

--- frontend/src/Componentes/PaginaNoticiaDesarrollada/PaginaNoticiaDesarrollada.js
+++ frontend/src/Componentes/PaginaNoticiaDesarrollada/PaginaNoticiaDesarrollada.js
@@ -5,6 +5,7 @@ import {SliderNoticias} from '../SliderNoticias/SliderNoticias';
 import NoticiasMiniatura from '../NoticiasMiniatura/NoticiasMiniatura';
 import publicidadLarga from '../../Static/Img/Publicidad larga.jpg';
 import publicidadCorta from '../../Static/Img/publicidad corta.jpg';
+import {useSelector} from 'react-redux';
 
 const PaginaNoticiaDesarrollada = ({tituloSeccionNoticias = 'Noticia Desarrollada'}) => {
   const userAgent = navigator.userAgent;
@@ -12,20 +13,19 @@ const PaginaNoticiaDesarrollada = ({tituloSeccionNoticias = 'Noticia Desarrollad
   const isMobileAndroid = userAgent.indexOf('Android');
 
   console.log(isMobileIPhone);
-  console.log(isMobileAndroid);
+  // console.log(isMobileAndroid);
+
+  const {noticias1} = useSelector(state => state.storePrueba);
+
+  console.log(noticias1);
   // console.log(userAgent);
 
   return (
     <div className="LI-ND-Noticia-Desarrollada Fondo-seccion-noticia-desarrollada">
       <div className="CP-ND-Noticias">
-        <div className="CI-ND-Noticia-titulo">
-          <div className="I-ND-Noticia-titulo">
-            <p>{tituloSeccionNoticias}</p>
-          </div>
-        </div>
         <div className="CI-ND-Noticia-general">
           <div className="I-Noticia-desarrollada-Componente">
-            <NoticiaDesarrollada />
+            <NoticiaDesarrollada datosModelado={noticias1} />
           </div>
         </div>
         <div className="CI-ND-Noticia-miniatura">
@@ -33,8 +33,8 @@ const PaginaNoticiaDesarrollada = ({tituloSeccionNoticias = 'Noticia Desarrollad
             <img alt="" src={publicidadCorta}></img>
           </div>
           <div className="I-ND-Noticia-Componente-Slider">
-            {isMobileAndroid !== -1 ? (
-              <SliderNoticias cantidadDeElementos={3} isVertical={false} />
+            {isMobileAndroid !== -1 || isMobileIPhone !== -1 ? (
+              <SliderNoticias cantidadDeElementos={6} isVertical={false} />
             ) : (
               <SliderNoticias cantidadDeElementos={3} isVertical={true} />
             )}
