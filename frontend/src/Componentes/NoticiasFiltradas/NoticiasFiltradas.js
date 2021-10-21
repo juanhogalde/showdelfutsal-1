f5fd3e7156cd60282971a2c7ecf22d46c461aef8
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-21 16:04:45 -0300

    Feat: Noticias filtradas

--- frontend/src/Componentes/NoticiasFiltradas/NoticiasFiltradas.js
+++ frontend/src/Componentes/NoticiasFiltradas/NoticiasFiltradas.js
@@ -10,14 +10,18 @@ const NoticiasFiltradas = () => {
           <p>TITULO</p>
         </div>
       </div>
-      <div className="CI-SN-Noticia-filtrada-grilla">
+      <div className="CI-SN-Noticia-filtrada-grilla CI-SN-Noticia-filtrada-auto-grilla">
         <div className="I-Noticia-filtrada-grilla">
           <NoticiasMiniatura />
+        </div>
+        <div className="I-Noticia-filtrada-grilla">
           <NoticiasMiniatura />
-          <NoticiasMiniatura />
+        </div>
+        <div className="I-Noticia-filtrada-grilla">
           <NoticiasMiniatura />
         </div>
       </div>
+      <div className="CI-SN-Noticia-filtrada-Panel-lateral"></div>
     </div>
   );
 };
