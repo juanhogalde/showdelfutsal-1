a577dfb9de7de83da2fab4efa3eb50ff06493528
MaxiLowa <maximiliano@lowa.com.ar>
2021-10-27 11:39:28 -0300

    feat: Corrección de footer.

--- frontend/src/Componentes/HomePublica/HomePublica.js
+++ frontend/src/Componentes/HomePublica/HomePublica.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import RutasPublicas from '../../Rutas/RutasPublicas';
 import BarraDeNavegacion from '../BarraDeNavegacion/BarraDeNavegacion';
-/* import PieDepagina from '../PieDePagina/PieDepagina'; */
 
 export const HomePublica = () => {
   return (
     <React.Fragment>
       <BarraDeNavegacion></BarraDeNavegacion>
       <RutasPublicas />
-      {/* <PieDepagina></PieDepagina> */}
     </React.Fragment>
   );
 };
