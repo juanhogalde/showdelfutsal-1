d26393fe6a8ba85100138f3881f09ec83d3e52cd
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-28 14:15:17 -0300

    Componente menu lateral

---
+++ frontend/src/ComponentesAdmin/NavegacionLateral/NavegacionLateral.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavegacionLateral.css';
+
+const NavegacionLateral = () => {
+  return (
+    <div className="CP-Navegacion-lateral">
+      <div className="CI-Usuario"></div>
+      <div className="CI-Navegacion-Lateral">
+        <div className="I-Navegacion-Lateral-Menu"></div>
+        <div className="I-Navegacion-Lateral-Menu"></div>
+        <div className="I-Navegacion-Lateral-Menu"></div>
+      </div>
+    </div>
+  );
+};
+export default NavegacionLateral;
