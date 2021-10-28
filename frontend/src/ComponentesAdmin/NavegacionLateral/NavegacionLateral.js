8f094f289f6d5626988a42e3db88b394909f671e
IgnacioLowa <ignacio@lowa.com.ar>
2021-10-28 15:54:14 -0300

    Navegacion lateral

--- frontend/src/ComponentesAdmin/NavegacionLateral/NavegacionLateral.js
+++ frontend/src/ComponentesAdmin/NavegacionLateral/NavegacionLateral.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import './NavegacionLateral.css';
+import {NavLink} from 'react-router-dom';
+import iconoPlus from '../../Static/Admin/plus.svg';
+import iconoAvatar from '../../Static/Admin/iconoAvatar.svg';
 
 const NavegacionLateral = () => {
   return (
     <div className="CP-Navegacion-lateral">
-      <div className="CI-Usuario"></div>
+      <div className="CI-Usuario">
+        <div className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoAvatar}></img>
+          <div className="I-Usuario">
+            <h4>Nombre usuario</h4>
+            <h6>Rol</h6>
+          </div>
+        </div>
+      </div>
       <div className="CI-Navegacion-Lateral">
-        <div className="I-Navegacion-Lateral-Menu"></div>
-        <div className="I-Navegacion-Lateral-Menu"></div>
-        <div className="I-Navegacion-Lateral-Menu"></div>
+        <NavLink exact to="/a" className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoPlus}></img>
+          <div className="I-Navegacion-Lateral-Link">
+            <h6>Fixture</h6>
+          </div>
+        </NavLink>
+        <NavLink exact to="/" className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoPlus}></img>
+          <div className="I-Navegacion-Lateral-Link">
+            <h6>Tabla de posiciones</h6>
+          </div>
+        </NavLink>
+        <NavLink exact to="/" className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoPlus}></img>
+          <div className="I-Navegacion-Lateral-Link">
+            <h6>Publicidad</h6>
+          </div>
+        </NavLink>
+        <NavLink exact to="/" className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoPlus}></img>
+          <div className="I-Navegacion-Lateral-Link">
+            <h6>Agregar Noticia</h6>
+          </div>
+        </NavLink>
+        <NavLink exact to="/" className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoPlus}></img>
+          <div className="I-Navegacion-Lateral-Link">
+            <h6>Agregar Imagen</h6>
+          </div>
+        </NavLink>
+        <NavLink exact to="/" className="I-Navegacion-Lateral-Menu">
+          <img className="icon-navegacion-lateral" alt="" src={iconoAvatar}></img>
+          <div className="I-Navegacion-Lateral-Link">
+            <h6>Mi Perfil</h6>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
