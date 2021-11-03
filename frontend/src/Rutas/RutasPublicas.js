31530b1411953e13f1f479f5bbfdde5f34450b9f
SergioLowa <sergio@lowa.com.ar>
2021-11-03 04:28:48 -1100

    front - implementacion de rutas de manera correcta

--- frontend/src/Rutas/RutasPublicas.js
+++ frontend/src/Rutas/RutasPublicas.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router';
+import {Redirect, Route, useLocation} from 'react-router';
 import {Switch} from 'react-router-dom';
 import PaginaSomos from '../Componentes/PaginaSomos/PaginaSomos';
 import Inicio from '../Componentes/Inicio/Inicio';
@@ -7,17 +7,24 @@ import PaginaNoticias from '../Componentes/PaginaNoticias/PaginaNoticias';
 import PaginaSecciones from '../Componentes/PaginaSecciones/PaginaSecciones';
 import PaginaNoticiaDesarrollada from '../Componentes/PaginaNoticiaDesarrollada/PaginaNoticiaDesarrollada';
 import NoticiasFiltradas from '../Componentes/NoticiasFiltradas/NoticiasFiltradas';
+import BarraDeNavegacion from '../Componentes/BarraDeNavegacion/BarraDeNavegacion';
+import Home from '../Componentes/Home/Home';
 const RutasPublicas = () => {
   return (
     <React.Fragment>
+      {!useLocation().pathname.includes('/Administrador') && <BarraDeNavegacion />}
       <Switch>
         <Route exact component={Inicio} path="/"></Route>
+        <Route exact component={Home} path="/Administrador"></Route>
         <Route exact component={PaginaSomos} path="/Somos"></Route>
         <Route exact component={PaginaNoticias} path="/Noticias"></Route>
         <Route exact component={PaginaNoticiaDesarrollada} path="/Noticia/Desarrollada"></Route>
         <Route exact component={PaginaSecciones} path="/Seccion"></Route>
         <Route exact component={PaginaSecciones} path="/Seccion/:id"></Route>
         <Route exact component={NoticiasFiltradas} path="/Noticias/Filtradas"></Route>
+        <Route path="*">
+          <Redirect to="/"></Redirect>
+        </Route>
       </Switch>
     </React.Fragment>
   );
