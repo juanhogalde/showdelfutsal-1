206dfc6ff1fe97611005a9e644715100695389f1
AlexisLowa <alexis@lowa.com.ar>
2021-11-08 08:52:55 -0300

    Revisión de modeos e interfaces

--- backend/src/Componentes/Tablas/Tablas_Model.ts
+++ backend/src/Componentes/Tablas/Tablas_Model.ts
@@ -1,43 +1,25 @@
-import {model, Schema} from 'mongoose';
+import { model, Schema } from 'mongoose';
 import ITablas from './Tablas_Interface';
 
 const TablasSchema = new Schema({
-  idEquipos: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'modeloEquipos',
-    },
-  ],
-  // equipo1: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'modeloEquipos',
-  // },
-  // equipo2: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'modeloEquipos',
-  // },
-  // isEquipo2Eliminado: {type: Boolean},
-  idCampeonato: {
-    type: Schema.Types.ObjectId,
-    ref: 'modeloCampeonatos',
-  },
-  zona: {type: String},
-  tipoZona: {type: Number},
-  // pGanados: {type: Number},
-  // pEmpatados: {type: Number},
-  // pPerdidos: {type: Number},
-  // pJugados: {type: Number},
-  // golesAFavor: {type: Number},
-  // golesEnContra: {type: Number},
-  // difGoles: {type: Number},
-  // puntos: {type: Number},
-  posicionEnTabla: {type: Number},
-  comentarios: [
-    {
-      color: {type: String},
-      texto: {type: String},
-    },
-  ],
+	idEquipos: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'modeloEquipos',
+		},
+	],
+	idCampeonato: {
+		type: Schema.Types.ObjectId,
+		ref: 'modeloCampeonatos',
+	},
+	zona: { type: String },
+	tipoZona: { type: Number },
+	comentarios: [
+		{
+			color: { type: String },
+			texto: { type: String },
+		},
+	],
 });
 
 export default model<ITablas>('modeloTablas', TablasSchema);
