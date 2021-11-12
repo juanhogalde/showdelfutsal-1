6bbfd493b2494df42280f9b33de9483753f20d1b
AlexisLowa <alexis@lowa.com.ar>
2021-11-12 15:31:32 -0300

    Subida de build por ftp automatizado

--- frontend/subirftp.bat
+++ frontend/subirftp.bat
@@ -1,6 +1,6 @@
 @echo off
-echo user u556865521.ignacio> ftpcmd.dat
-echo NachoLowa2021>> ftpcmd.dat
+echo user u556865521.futsal> ftpcmd.dat
+echo Lowa.S3rv3r>> ftpcmd.dat
 echo bin>> ftpcmd.dat
 echo put build.rar>> ftpcmd.dat
 echo quit>> ftpcmd.dat
