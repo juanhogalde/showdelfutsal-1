6315a7c1983c65831622f7d8a8359bd610eef3a2
AlexisLowa <alexis@lowa.com.ar>
2021-11-15 22:14:31 -0300

    Agregando configuraciones al archivo gulp

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -1,6 +1,10 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+import gulp from 'gulp';
+import pngquant from 'imagemin-pngquant';
+import imagemin from 'gulp-imagemin';
 
 gulp.task('imagemin', () => {
-  gulp.src('/public/imagenes/*').pipe(imagemin()).pipe(gulp.dest('/public/imagenes/'));
+	gulp
+		.src('/public/imagenes/*')
+		.pipe(imagemin([pngquant({ quality: '40' })]))
+		.pipe(gulp.dest('/public/imagenes/'));
 });
