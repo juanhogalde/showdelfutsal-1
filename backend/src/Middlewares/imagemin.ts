87718e1a5a0e41155560f2863baf207b55b8a7a2
AlexisLowa <alexis@lowa.com.ar>
2021-11-16 13:49:22 -0300

    Limpieza de código en función imagemin

--- backend/src/Middlewares/imagemin.ts
+++ backend/src/Middlewares/imagemin.ts
@@ -1,12 +1,10 @@
 const compress_images = require('compress-images');
 import fs from 'fs';
 
-const INPUT_path_to_your_images = 'public/cargaImagenes/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+// const INPUT_path_to_your_images = 'public/cargaImagenes/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
 const OUTPUT_path = 'public/imagenes/';
 
 export const comprimirImagen = (pathFile?: string) => {
-  // let pathImage: string = ``;
-  // INPUT_path_to_your_images,
   const pr = new Promise((resolve: any, reject: any) => {
     compress_images(
       pathFile,
