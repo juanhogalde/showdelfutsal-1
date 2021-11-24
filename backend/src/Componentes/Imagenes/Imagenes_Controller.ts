da3eed737b7f79ed1ea21149b53eb9c1a29d5391
AlexisLowa <alexis@lowa.com.ar>
2021-11-24 13:47:36 -0300

    QUitando del package.json compress-image y comentando su funcionalidad

--- backend/src/Componentes/Imagenes/Imagenes_Controller.ts
+++ backend/src/Componentes/Imagenes/Imagenes_Controller.ts
@@ -1,109 +1,121 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import responder from '../../Middlewares/responder';
 import modeloImagenes from './Imagenes_Model';
 import IImagenes from './Imagenes_Interface';
-import {comprimirImagen} from '../../Middlewares/imagemin';
+// import { comprimirImagen } from '../../Middlewares/imagemin';
 
 class ImagenesController {
-  public async listar(req: Request, res: Response) {
-    try {
-      const listadoImagenes = await modeloImagenes.find();
-      responder.sucess(req, res, listadoImagenes);
-    } catch (error) {
-      responder.error(req, res, error);
-    }
-  }
+	public async listar(req: Request, res: Response) {
+		try {
+			const listadoImagenes = await modeloImagenes.find();
+			responder.sucess(req, res, listadoImagenes);
+		} catch (error) {
+			responder.error(req, res, error);
+		}
+	}
 
-  public async agregar(req: Request, res: Response) {
-    try {
-      const imagen: IImagenes = new modeloImagenes({...req.body, fuente: req.body.archivos.path});
-      await imagen.save();
-      responder.sucess(req, res, imagen);
-    } catch (error) {
-      responder.error(req, res, error);
-    }
-  }
+	public async agregar(req: Request, res: Response) {
+		try {
+			const imagen: IImagenes = new modeloImagenes({
+				...req.body,
+				fuente: req.body.archivos.path,
+			});
+			await imagen.save();
+			responder.sucess(req, res, imagen);
+		} catch (error) {
+			responder.error(req, res, error);
+		}
+	}
 
-  public async cargarImagenPrueba(req: Request, res: Response) {
-    try {
-      if (!req.body) {
-        responder.error(req, res, 'No se ingresaron datos');
-      } else {
-        let path: string = req.body.archivos.path.split('\\');
-        let pathFileAComprimir: string = `${path[0]}/${path[1]}/${path[2]}`;
-        let resultado: any = await comprimirImagen(pathFileAComprimir);
+	// public async cargarImagenPrueba(req: Request, res: Response) {
+	//   try {
+	//     if (!req.body) {
+	//       responder.error(req, res, 'No se ingresaron datos');
+	//     } else {
+	//       let path: string = req.body.archivos.path.split('\\');
+	//       let pathFileAComprimir: string = `${path[0]}/${path[1]}/${path[2]}`;
+	//       let resultado: any = await comprimirImagen(pathFileAComprimir);
 
-        if (resultado) {
-          const imagen: IImagenes = new modeloImagenes({
-            ...req.body,
-            fuente: resultado.path_out_new,
-          });
-          await imagen.save();
-          responder.sucess(req, res, imagen);
-        } else {
-          responder.error(req, res, new Error('Ocurrió un error al insertar la imagen'));
-        }
-      }
-    } catch (error) {
-      responder.error(req, res, error);
-    }
-  }
+	//       if (resultado) {
+	//         const imagen: IImagenes = new modeloImagenes({
+	//           ...req.body,
+	//           fuente: resultado.path_out_new,
+	//         });
+	//         await imagen.save();
+	//         responder.sucess(req, res, imagen);
+	//       } else {
+	//         responder.error(req, res, new Error('Ocurrió un error al insertar la imagen'));
+	//       }
+	//     }
+	//   } catch (error) {
+	//     responder.error(req, res, error);
+	//   }
+	// }
 
-  public async obtener(req: Request, res: Response) {
-    try {
-      let idImagen = req.params.id;
-      const imagen = await modeloImagenes.find({_id: idImagen});
-      responder.sucess(req, res, imagen);
-    } catch (error) {
-      responder.error(req, res, error);
-    }
-  }
+	public async obtener(req: Request, res: Response) {
+		try {
+			let idImagen = req.params.id;
+			const imagen = await modeloImagenes.find({ _id: idImagen });
+			responder.sucess(req, res, imagen);
+		} catch (error) {
+			responder.error(req, res, error);
+		}
+	}
 
-  public async modificar(req: Request, res: Response) {
-    try {
-      const imagenBody = req.body;
-      if (imagenBody._id) {
-        modeloImagenes.findById(imagenBody._id).then(async (imagen: any) => {
-          if (imagen) {
-            imagen.fuente = imagenBody.fuente;
-            imagen.alto = imagenBody.alto;
-            imagen.ancho = imagenBody.ancho;
-            imagen.descripcion = imagenBody.descripcion;
-            imagen.galeria = imagenBody.galeria;
-            imagen.fechaCarga = imagenBody.fechaCarga;
+	public async modificar(req: Request, res: Response) {
+		try {
+			const imagenBody = req.body;
+			if (imagenBody._id) {
+				modeloImagenes.findById(imagenBody._id).then(async (imagen: any) => {
+					if (imagen) {
+						imagen.fuente = imagenBody.fuente;
+						imagen.alto = imagenBody.alto;
+						imagen.ancho = imagenBody.ancho;
+						imagen.descripcion = imagenBody.descripcion;
+						imagen.galeria = imagenBody.galeria;
+						imagen.fechaCarga = imagenBody.fechaCarga;
 
-            const resultado = await imagen.save({new: true});
-            responder.sucess(req, res, resultado);
-          } else {
-            let error = new Error('Imagen no encontrada');
-            responder.error(req, res, error);
-          }
-        });
-      } else {
-        let error = new Error('Imagen no encontrada');
-        responder.error(req, res, error);
-      }
-    } catch (error) {
-      responder.error(req, res, error);
-    }
-  }
+						const resultado = await imagen.save({ new: true });
+						responder.sucess(req, res, resultado);
+					} else {
+						let error = new Error('Imagen no encontrada');
+						responder.error(req, res, error);
+					}
+				});
+			} else {
+				let error = new Error('Imagen no encontrada');
+				responder.error(req, res, error);
+			}
+		} catch (error) {
+			responder.error(req, res, error);
+		}
+	}
 
-  public async eliminar(req: Request, res: Response) {
-    try {
-      let id = req.body.id;
-      const imagenEliminada = await modeloImagenes.findOneAndDelete({_id: id}, {new: true});
-      responder.sucess(req, res, imagenEliminada);
-    } catch (error) {
-      responder.error(req, res, error);
-    }
-  }
+	public async eliminar(req: Request, res: Response) {
+		try {
+			let id = req.body.id;
+			const imagenEliminada = await modeloImagenes.findOneAndDelete(
+				{ _id: id },
+				{ new: true }
+			);
+			responder.sucess(req, res, imagenEliminada);
+		} catch (error) {
+			responder.error(req, res, error);
+		}
+	}
 
-  public async obtenerGaleria(nombreGaleria: string) {
-    return modeloImagenes.find({galeria: nombreGaleria}).sort({fechaCarga: 'desc'}).limit(3);
-  }
+	public async obtenerGaleria(nombreGaleria: string) {
+		return modeloImagenes
+			.find({ galeria: nombreGaleria })
+			.sort({ fechaCarga: 'desc' })
+			.limit(3);
+	}
 
-  public async obtenerGaleriaVideo(nombreGaleria: string) {
-    return modeloImagenes.find({galeriaVideo: nombreGaleria}).sort({fechaCarga: 'desc'}).limit(2);
-  }
+	public async obtenerGaleriaVideo(nombreGaleria: string) {
+		return modeloImagenes
+			.find({ galeriaVideo: nombreGaleria })
+			.sort({ fechaCarga: 'desc' })
+			.limit(2);
+	}
 }
 export const imagenesController = new ImagenesController();
