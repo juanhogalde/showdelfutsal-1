064692a2ea8002aa2e53eaf3c000c437af7fd7d9
SergioLowa <sergio@lowa.com.ar>
2021-12-02 11:05:56 -0300

    escudos

--- frontend/src/Redux/Partidos/ReducerPartidos.js
+++ frontend/src/Redux/Partidos/ReducerPartidos.js
@@ -1,15 +1,15 @@
 import {} from './AccionPartidos';
-import {urlImagenes} from '../../urlImagenes';
+import {urlStaticImages} from '../../urlImagenes';
 const partidosPorDefecto = {
   partidos: [
     {
       equipoLocal: {
         nombreClub: 'Alianza',
-        escudo: urlImagenes + '/imagenes/zCom5ZOiRbCuBmvMuXfFFgnB.png',
+        escudo: urlStaticImages + 'escudo_alianza.fe7f5b67.png',
       },
       equipoVisitante: {
         nombreClub: 'Trinidad',
-        escudo: urlImagenes + '/imagenes/yY9zTQq_fhQ565szhmE8yDZc.png',
+        escudo: urlStaticImages + 'escudo_huarpes.3dc53784.png',
       },
       resultadoLocal: 6,
       resultadoVisitante: 6,
@@ -31,64 +31,64 @@ const partidosPorDefecto = {
       },
       idTabla: {},
     },
-    {
-      equipoLocal: {
-        nombreClub: 'Villa Hipódromo',
-        escudo: urlImagenes + '/imagenes/qY7P0Olc6xtCfHvf9bL8o1nI.png',
-      },
-      equipoVisitante: {
-        nombreClub: 'Aberastain San Lorenzo',
-        escudo: urlImagenes + '/imagenes/rb7mFK9aRYpvMrbiRx21CUu4.jpeg',
-      },
-      resultadoLocal: 6,
-      resultadoVisitante: 6,
-      penalesLocal: '4',
-      penalesVisitante: '2',
-      fechaPartido: '2021-12-20T16:00:00',
-      fecha: 'Primera Fecha',
-      idEstadio: {
-        nombreEstadio: 'Estadio Bicentenario',
-        direccion: 'calle',
-      },
-      posicionFixture: '',
-      comentarios: [],
-      campeonato: {
-        tituloCampeonato: 'copa 2021',
-        fechaInicio: '2021-10-26T17:58:06.719+00:00',
-        fechaFin: '2021-11-26T17:58:06.719+00:00',
-        keyCategoria: 1,
-      },
-      idTabla: {},
-    },
-    {
-      equipoLocal: {
-        nombreClub: 'Alianza 2',
-        escudo: urlImagenes + '/imagenes/BBwI4rWo3OUvFW0hjOJjFOHl.png',
-      },
-      equipoVisitante: {
-        nombreClub: 'Trinidad 2',
-        escudo: urlImagenes + '/imagenes/xcDE-dLcIaiCPyCvtcx5_Vt6.png',
-      },
-      resultadoLocal: 2,
-      resultadoVisitante: 2,
-      penalesLocal: '0',
-      penalesVisitante: '0',
-      fechaPartido: '2021-12-10T22:00:00',
-      fecha: 'Primera Fecha',
-      idEstadio: {
-        nombreEstadio: 'Estadio Aldo Cantoni 2',
-        direccion: 'urquiza 2',
-      },
-      posicionFixture: '',
-      comentarios: [],
-      campeonato: {
-        tituloCampeonato: 'final 2021 - 2',
-        fechaInicio: '2021-02-26T17:58:06.719+00:00',
-        fechaFin: '2021-11-26T17:58:06.719+00:00',
-        keyCategoria: 2,
-      },
-      idTabla: {},
-    },
+    // {
+    //   equipoLocal: {
+    //     nombreClub: 'Villa Hipódromo',
+    //     escudo: urlStaticImages + 'escudo_cvhd.png',
+    //   },
+    //   equipoVisitante: {
+    //     nombreClub: 'Aberastain San Lorenzo',
+    //     escudo: urlStaticImages + 'escudo_aberastain.jpeg',
+    //   },
+    //   resultadoLocal: 6,
+    //   resultadoVisitante: 6,
+    //   penalesLocal: '4',
+    //   penalesVisitante: '2',
+    //   fechaPartido: '2021-12-20T16:00:00',
+    //   fecha: 'Primera Fecha',
+    //   idEstadio: {
+    //     nombreEstadio: 'Estadio Bicentenario',
+    //     direccion: 'calle',
+    //   },
+    //   posicionFixture: '',
+    //   comentarios: [],
+    //   campeonato: {
+    //     tituloCampeonato: 'copa 2021',
+    //     fechaInicio: '2021-10-26T17:58:06.719+00:00',
+    //     fechaFin: '2021-11-26T17:58:06.719+00:00',
+    //     keyCategoria: 1,
+    //   },
+    //   idTabla: {},
+    // },
+    // {
+    //   equipoLocal: {
+    //     nombreClub: 'Alianza 2',
+    //     escudo: urlStaticImages + 'BBwI4rWo3OUvFW0hjOJjFOHl.png',
+    //   },
+    //   equipoVisitante: {
+    //     nombreClub: 'Trinidad 2',
+    //     escudo: urlStaticImages + 'xcDE-dLcIaiCPyCvtcx5_Vt6.png',
+    //   },
+    //   resultadoLocal: 2,
+    //   resultadoVisitante: 2,
+    //   penalesLocal: '0',
+    //   penalesVisitante: '0',
+    //   fechaPartido: '2021-12-10T22:00:00',
+    //   fecha: 'Primera Fecha',
+    //   idEstadio: {
+    //     nombreEstadio: 'Estadio Aldo Cantoni 2',
+    //     direccion: 'urquiza 2',
+    //   },
+    //   posicionFixture: '',
+    //   comentarios: [],
+    //   campeonato: {
+    //     tituloCampeonato: 'final 2021 - 2',
+    //     fechaInicio: '2021-02-26T17:58:06.719+00:00',
+    //     fechaFin: '2021-11-26T17:58:06.719+00:00',
+    //     keyCategoria: 2,
+    //   },
+    //   idTabla: {},
+    // },
   ],
   isPartido: {isMostrar: false, tipo: '', mensaje: '', isExito: false, isError: false},
 };
--- frontend/src/urlImagenes.js
+++ frontend/src/urlImagenes.js
@@ -1,2 +1,3 @@
 import {api} from '../src/api';
 export const urlImagenes = `http://${api}:4000`;
+export const urlStaticImages = `http://futsal.lowa.com.ar/static/media/`; // en local http://localhost/Static/Escudos/
