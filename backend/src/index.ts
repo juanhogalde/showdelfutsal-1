dc8af8aba2c16ea18c8b672e934cd2889e426553
SergioLowa <sergio@lowa.com.ar>
2021-12-02 12:04:53 -0300

    modificacndo reducer partidos y api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,7 @@ import {instalarBD} from './Config/instalacionInicial';
 process.env.NODE_ENV = process.env.NODE_ENV || 'desarrollo';
 
 ///// DEPLOY
-const deploy = 'v0.0.4';
+const deploy = 'v0.0.6';
 
 class Server {
   public app: express.Application;
--- frontend/src/urlImagenes.js
+++ frontend/src/urlImagenes.js
@@ -1,3 +1,3 @@
 import {api} from '../src/api';
 export const urlImagenes = `http://${api}:4000`;
-export const urlStaticImages = `http://futsal.lowa.com.ar/static/media/`; // en local http://localhost/Static/Escudos/
+export const urlStaticImages = `http://futsal.lowa.com.ar/escudos/`; // en local http://localhost/Static/Escudos/
