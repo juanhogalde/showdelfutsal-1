e4179ffd27b8c7519bcf7f13a353de81efd9b24e
SergioLowa <sergio@lowa.com.ar>
2021-12-14 12:41:17 -0300

    Implementar eliminar

--- frontend/src/ComponentesAdmin/TarjetaNoticias/TarjetaNoticias.js
+++ frontend/src/ComponentesAdmin/TarjetaNoticias/TarjetaNoticias.js
@@ -1,54 +1,110 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TarjetaNoticias.css';
 import ImagenAdmin from '../ImagenAdmin/ImagenAdmin';
 import TarjetaNoticiasMiniatura from '../TarjetaNoticiasMiniatura/TarjetaNoticiasMiniatura';
-import {/*BiDotsVerticalRounded*/ BiPen, BiTrash} from 'react-icons/bi';
-import {useDispatch} from 'react-redux';
+
+import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
-import {guardarNoticiaParaEditar_accion} from '../../Redux/Noticias/AccionesNoticias';
+import {
+  actualizarListaNoticias_accion,
+  eliminarNoticia_accion,
+  guardarNoticiaParaEditar_accion,
+  volverPorDefecto_accion,
+} from '../../Redux/Noticias/AccionesNoticias';
+import {useRef} from 'react';
+import {FiEdit3} from 'react-icons/fi';
+import {MdDeleteForever} from 'react-icons/md';
+import {HiDotsVertical} from 'react-icons/hi';
+import Alertas from '../Alertas/Alertas';
 // import {useSelector} from 'react-redux';
 
 export const TarjetaNoticias = ({noticia = {}}) => {
   const historialDeNavegacion = useHistory();
   const dispatch = useDispatch();
-  const accionesOpciones = (noticia, tipoAccion) => {
+  const [isAcciones, setIsAcciones] = useState(false);
+  const {isNoticiaGurdada} = useSelector(state => state.storeNoticias);
+  const [mostrarAlertEliminar, setMostrarAlertEliminar] = useState(false);
+  const elementoAcciones = useRef();
+  const mostrarAcciones = () => {
+    setIsAcciones(!isAcciones);
+    elementoAcciones.current.focus();
+  };
+  const ocultarAcciones = () => {
+    setIsAcciones(false);
+  };
+  const accionesOpciones = tipoAccion => {
     if (tipoAccion === 'editar') {
-      console.log(tipoAccion);
       dispatch(guardarNoticiaParaEditar_accion(noticia));
       historialDeNavegacion.push(`/Noticia/Editar`);
     } else {
+      setMostrarAlertEliminar(true);
     }
   };
-  // const {noticias} = useSelector(state => state.storeNoticias);
+  const opcionSeleccionada = respuesta => {
+    if (respuesta) {
+      setMostrarAlertEliminar(false);
+      dispatch(eliminarNoticia_accion(noticia));
+    } else {
+      setMostrarAlertEliminar(false);
+    }
+  };
+  const respuestaDeAlerta = () => {
+    if (isNoticiaGurdada.isEliminado) {
+      dispatch(actualizarListaNoticias_accion(noticia._id));
+    } else {
+      dispatch(volverPorDefecto_accion());
+    }
+  };
 
-  // return noticias.map(noticia => {
   return (
     <div key={noticia._id} className="CP-Tarjeta-Noticias-Admin">
-      <div className="CI-Tarjeta-Noticias-Admin">
+      <div className="CI-Tarjeta-Noticias-Admin ">
         <div className="I-Tarjeta-Noticias-Imagen">
           <ImagenAdmin noticiaImagen={noticia} />
         </div>
         <div className="I-Tarjeta-Noticias-Cuerpo">
           <TarjetaNoticiasMiniatura noticiaRecibida={noticia} />
         </div>
-        <div className="I-Tarjeta-Noticias-Opciones">
-          <BiPen
-            onClick={() => {
-              accionesOpciones(noticia, 'editar');
-            }}
-            size={40}
-          />
-          <BiTrash
-            onClick={() => {
-              accionesOpciones(noticia, 'eliminar');
-            }}
-            size={40}
-          />
+        <div className="acciones-TarjetaNoticia">
+          <HiDotsVertical onClick={() => mostrarAcciones()} />
+          <div
+            ref={elementoAcciones}
+            id="acciones-TarjetaNoticia"
+            className={`${
+              isAcciones
+                ? 'CI-Acciones-TarjetaNoticia CI-Acciones-TarjetaNoticia-Apertura'
+                : 'CI-Acciones-TarjetaNoticia'
+            }`}
+            tabIndex="1"
+            onBlur={() => ocultarAcciones()}
+          >
+            <FiEdit3
+              className="iconoAcción-ListaImagenes"
+              onClick={() => accionesOpciones('editar')}
+            ></FiEdit3>
+            <MdDeleteForever
+              onClick={() => accionesOpciones('eliminar')}
+              className="iconoAcción-ListaImagenes"
+            />
+          </div>
         </div>
       </div>
+      <Alertas
+        mostrarSweet={mostrarAlertEliminar}
+        tipoDeSweet="info"
+        subtitulo="Eliminar noticia"
+        RespuestaDeSweet={opcionSeleccionada}
+      />
+      <Alertas
+        mostrarSweet={
+          isNoticiaGurdada.isEliminado || isNoticiaGurdada.isError || isNoticiaGurdada.isMostrar
+        }
+        tipoDeSweet={isNoticiaGurdada.tipo}
+        subtitulo={isNoticiaGurdada.mensaje}
+        RespuestaDeSweet={respuestaDeAlerta}
+      />
     </div>
   );
-  // });
 };
 
 export default TarjetaNoticias;
