57b0ff6e33ea4cfc54b98ea8dbc9cc2d171dc56e
SergioLowa <sergio@lowa.com.ar>
2021-12-16 10:16:40 -0300

    Implementar Comprimir - imagen en agregar noticia y publicidad

--- frontend/src/ComponentesAdmin/NuevaNoticia/NuevaNoticia.js
+++ frontend/src/ComponentesAdmin/NuevaNoticia/NuevaNoticia.js
@@ -15,6 +15,7 @@ import Alertas from '../Alertas/Alertas';
 import {useHistory} from 'react-router';
 import {useLayoutEffect} from 'react';
 import {urlImagenes} from '../../urlImagenes';
+import compresor from '../../ModulosExternos/Compresor';
 
 const NuevaNoticia = ({tituloBoton = 'Guardar', isNueva = true, isConsulta = false}) => {
   const history = useHistory();
@@ -25,8 +26,77 @@ const NuevaNoticia = ({tituloBoton = 'Guardar', isNueva = true, isConsulta = fal
   const [subCategoria, setSubCategoria] = useState();
   const [datosCargados, setdatosCargados] = useState({});
   const dispatch = useDispatch();
+  const [alertaComprimir, setAlertaComprimir] = useState({
+    tipo: '',
+    mensaje: '',
+    isCargando: false,
+    isExito: false,
+    isError: false,
+  });
+  const [isErrorAlComprimir, setIsErrorAlComprimir] = useState(false);
   const escucharCambios = (name, value) => {
-    setdatosCargados({...datosCargados, [name]: value});
+    if (name === 'imagen') {
+      if (value.length > 0) {
+        /* setCantidadDeArchivos(value.length + datosGaleria.imagenes.length); */
+        setAlertaComprimir({
+          tipo: 'cargando',
+          mensaje: 'Comprimiendo Imagen...',
+          isCargando: true,
+          isExito: false,
+          isError: false,
+        });
+        let aux = [];
+        Object.values(value).forEach(async img => {
+          const respuesta = compresor(img);
+          const resultado = await respuesta
+            .then(res => {
+              setAlertaComprimir({
+                tipo: 'success',
+                mensaje: 'Imagen comprimida con éxito.',
+                isCargando: false,
+                isExito: true,
+                isError: false,
+              });
+              return res;
+            })
+            .catch(error => {
+              console.log(error);
+              setAlertaComprimir({
+                tipo: 'error',
+                mensaje: 'No se logró comprimir la imagen.',
+                isCargando: false,
+                isExito: false,
+                isError: true,
+              });
+              setIsErrorAlComprimir(true);
+            });
+
+          aux = [...aux, resultado];
+
+          setdatosCargados({...datosCargados, imagen: aux});
+        });
+      }
+    } else {
+      setdatosCargados({...datosCargados, [name]: value});
+    }
+  };
+  const respuestaDeSweetAlComprimir = respuesta => {
+    if (respuesta) {
+      setAlertaComprimir({
+        tipo: '',
+        mensaje: '',
+        isCargando: false,
+        isExito: false,
+        isError: false,
+      });
+    }
+    if (isErrorAlComprimir) {
+      setdatosCargados({
+        ...datosCargados,
+        imagen: [],
+      });
+      setIsErrorAlComprimir(false);
+    }
   };
   useLayoutEffect(() => {
     if (!isNueva) {
@@ -150,6 +220,14 @@ const NuevaNoticia = ({tituloBoton = 'Guardar', isNueva = true, isConsulta = fal
         tipoDeSweet={isNoticiaGurdada.tipo}
         RespuestaDeSweet={RespuestaDeAlertaVolverPorDefecto}
       />
+      <Alertas
+        tipoDeSweet={alertaComprimir.tipo}
+        mostrarSweet={
+          alertaComprimir.isCargando || alertaComprimir.isExito || alertaComprimir.isError
+        }
+        subtitulo={alertaComprimir.mensaje}
+        RespuestaDeSweet={respuestaDeSweetAlComprimir}
+      ></Alertas>
     </div>
   );
 };
