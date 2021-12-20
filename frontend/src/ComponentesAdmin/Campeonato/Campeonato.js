4f9d50464f2afda64ef6695cde4418c870461287
MaxiLowa <maximiliano@lowa.com.ar>
2021-12-20 14:17:57 -0300

    datos en Campeonato

--- frontend/src/ComponentesAdmin/Campeonato/Campeonato.js
+++ frontend/src/ComponentesAdmin/Campeonato/Campeonato.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import TarjetaTorneo from '../TarjetaTorneo/TarjetaTorneo';
 import './Campeonato.css';
 
 const Campeonato = () => {
-  return (
-    <div className="CP-Campeonato">
-      <div className="CI-CampeonatoMasculino">
-        <p>Masculino</p>
-        <TarjetaTorneo isCampeonato={true}></TarjetaTorneo>
-        <TarjetaTorneo isCampeonato={true}></TarjetaTorneo>
-        <TarjetaTorneo isCampeonato={true}></TarjetaTorneo>
-        <TarjetaTorneo isCampeonato={true}></TarjetaTorneo>
+  const {categorias, subcategorias} = useSelector(state => state.sotreDatosIniciales);
+
+  if (categorias.length > 0 && subcategorias.length > 0) {
+    return (
+      <div className="CP-Campeonato">
+        <div className="CI-CampeonatoMasculino">
+          <p>{categorias[0].label ? categorias[0].label : ''}</p>
+          {subcategorias.map((subcategoria, index) => {
+            return (
+              <TarjetaTorneo
+                datos={subcategoria.label}
+                key={index}
+                isCampeonato={true}
+              ></TarjetaTorneo>
+            );
+          })}
+        </div>
+        <div className="CI-CampeonatoMasculino">
+          <p>{categorias[2].label ? categorias[2].label : ''}</p>
+          {subcategorias.map((subcategoria, index) => {
+            if (subcategoria.key === 1 || subcategoria.key === 2) {
+              return (
+                <TarjetaTorneo
+                  datos={subcategoria.label}
+                  key={index}
+                  isCampeonato={true}
+                ></TarjetaTorneo>
+              );
+            } else return '';
+          })}
+        </div>
       </div>
-      <div className="CI-CampeonatoMasculino">
-        <p>Femenino</p>
-        <TarjetaTorneo isCampeonato={true}></TarjetaTorneo>
-        <TarjetaTorneo isCampeonato={true}></TarjetaTorneo>
+    );
+  } else {
+    return (
+      <div className="CP-Campeonato">
+        <h6>Cargando</h6>
       </div>
-    </div>
-  );
+    );
+  }
 };
 export default Campeonato;
