0aac6a5a0cf7b3a4fda21324551551c8164f5929
MaxiLowa <maximiliano@lowa.com.ar>
2021-12-21 10:32:57 -0300

    Feat: Maquetar pantalla enfrentamientos.

--- frontend/src/ComponentesAdmin/Enfrentamiento/Enfrentamiento.js
+++ frontend/src/ComponentesAdmin/Enfrentamiento/Enfrentamiento.js
@@ -1,24 +1,16 @@
 import React, {useState} from 'react';
 import './Enfrentamiento.css';
 import iconoFlecha from '../../Static/Admin/iconoAtras.svg';
-import InputLowa from '../InputLowa/InputLowa';
-import {FiClock} from 'react-icons/fi';
 import InputDateLowa from '../InputDateLowa/InputDateLowa';
 import Selector from '../Selector/Selector';
+import BotonLowa from '../BotonLowa/BotonLowa';
 
 const Enfrentamiento = () => {
   const [isMostrarCuerpo, setIsMostrarCuerpo] = useState(false);
-  const apertura = foco => {
-    console.log(foco);
-    if (foco) {
-      setIsMostrarCuerpo(!isMostrarCuerpo);
-    }
-  };
+
   return (
     <div className="CP-Enfrentamiento">
       <div
-        tabIndex="1"
-        /* onFocus={() => apertura(true)} */
         onClick={() => setIsMostrarCuerpo(!isMostrarCuerpo)}
         className={
           isMostrarCuerpo ? 'CI-Cabecera-Enfrentamiento borde' : 'CI-Cabecera-Enfrentamiento'
@@ -39,16 +31,16 @@ const Enfrentamiento = () => {
             ? 'CI-Cuerpo-Enfrentamiento aperutra-Enfrentamiento'
             : 'CI-Cuerpo-Enfrentamiento'
         }
-        tabIndex="2"
-        /* onFocus={() => apertura(false)} */
       >
         <div className="I-Datos-Enfrentamiento">
-          <Selector></Selector>
+          <Selector placeholder="Seleccione Equipo"></Selector>
+          <Selector placeholder="Seleccione Equipo"></Selector>
+
+          <InputDateLowa type="date" placeholder="Fecha de Enfrentamiento"></InputDateLowa>
+          <InputDateLowa type="time" placeholder="Hora de Enfrentamiento"></InputDateLowa>
 
           <Selector></Selector>
-
-          <InputDateLowa placeholder="Fecha de Enfrentamiento"></InputDateLowa>
-          <Selector></Selector>
+          <BotonLowa></BotonLowa>
         </div>
       </div>
     </div>
