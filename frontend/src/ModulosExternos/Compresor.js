a4dbe7f96867df0d802add7a436f6d8893e3814f
MaxiLowa <maximiliano@lowa.com.ar>
2021-12-29 09:44:40 -0300

    Controlando imágenes livianas enviadas a compresor

--- frontend/src/ModulosExternos/Compresor.js
+++ frontend/src/ModulosExternos/Compresor.js
@@ -1,16 +1,19 @@
 import Compressor from 'compressorjs';
 
 const compresor = async (archivo = {}, calidadCompresion = 0.8) => {
+  console.log(archivo);
   return await new Promise((resolve, reject) => {
-    new Compressor(archivo, {
-      quality: calidadCompresion,
-      success(archivoComprimido) {
-        resolve(archivoComprimido);
-      },
-      error(err) {
-        reject(err);
-      },
-    });
+    if (archivo.size > 1000000) {
+      new Compressor(archivo, {
+        quality: calidadCompresion,
+        success(archivoComprimido) {
+          resolve(archivoComprimido);
+        },
+        error(err) {
+          reject(err);
+        },
+      });
+    } else resolve(archivo);
   });
 };
 export default compresor;
