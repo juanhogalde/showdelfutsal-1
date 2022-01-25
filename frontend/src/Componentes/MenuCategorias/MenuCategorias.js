30f12713da581a3f3332739e88a44306dc0fd1c8
MaxiLowa <maximiliano@lowa.com.ar>
2022-01-25 16:28:26 -0300

    correcciones

--- frontend/src/Componentes/MenuCategorias/MenuCategorias.js
+++ frontend/src/Componentes/MenuCategorias/MenuCategorias.js
@@ -66,6 +66,13 @@ const MenuCategorias = props => {
       {categoria && categoria === 4 && (
         <div className="I-submenu" onClick={() => handleClick(categoria)}>
           <Link className="Links-submenu" to="/Seccion/9">
+            <h6>Ligas Departamentales</h6>
+          </Link>
+        </div>
+      )}
+      {categoria && categoria === 4 && (
+        <div className="I-submenu" onClick={() => handleClick(categoria)}>
+          <Link className="Links-submenu" to="/Seccion/9">
             <h6>Copas y Torneos</h6>
           </Link>
         </div>
