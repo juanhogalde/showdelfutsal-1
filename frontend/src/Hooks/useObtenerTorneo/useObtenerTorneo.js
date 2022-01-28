f55fafa22fe2c69d2f05332d06193457f846f160
MaxiLowa <maximiliano@lowa.com.ar>
2022-01-28 11:31:39 -0300

    useObtenerTorneo

---
+++ frontend/src/Hooks/useObtenerTorneo/useObtenerTorneo.js
@@ -0,0 +1,25 @@
+import {useLayoutEffect, useMemo, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {cargarDatosDeTorneoParaEdicion_accion} from '../../Redux/Torneos/AccionesTorneos';
+
+const useObtenerTorneo = id => {
+  const dispatch = useDispatch();
+  const torneo = useSelector(state => state.storeTorneos.torneos.find(torneo => torneo._id === id));
+  const [isTorneo, setIsTorneo] = useState(false);
+
+  useLayoutEffect(() => {
+    if (torneo) {
+      if (Object.keys(torneo).length > 0) {
+        dispatch(cargarDatosDeTorneoParaEdicion_accion(torneo));
+        console.log(torneo);
+        setIsTorneo(true);
+      } else {
+        setIsTorneo(false);
+      }
+    }
+    return () => {};
+  }, [torneo, dispatch]);
+
+  return isTorneo;
+};
+export default useObtenerTorneo;
