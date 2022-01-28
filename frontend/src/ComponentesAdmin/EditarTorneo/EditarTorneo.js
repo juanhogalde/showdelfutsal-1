802247c2a7639b62379cfa987a0d00b32e9437bf
MaxiLowa <maximiliano@lowa.com.ar>
2022-01-28 09:59:16 -0300

    implementación de persistencia de datos de un torneo

--- frontend/src/ComponentesAdmin/EditarTorneo/EditarTorneo.js
+++ frontend/src/ComponentesAdmin/EditarTorneo/EditarTorneo.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import './EditarTorneo.css';
 import Cargando from '../Cargando/Cargando';
 import {useParams} from 'react-router-dom';
 import NuevoTorneo from '../NuevoTorneo/NuevoTorneo';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {cargarDatosDeTorneoParaEdicion_accion} from '../../Redux/Torneos/AccionesTorneos';
 
 const EditarTorneo = () => {
   const {id} = useParams();
+  const dispatch = useDispatch();
   const torneo = useSelector(state => state.storeTorneos.torneos.find(torneo => torneo._id === id));
+  const [isTorneo, setIsTorneo] = useState(false);
 
-  if (true) {
+  useLayoutEffect(() => {
+    if (torneo) {
+      if (Object.keys(torneo).length > 0) {
+        setIsTorneo(true);
+        dispatch(cargarDatosDeTorneoParaEdicion_accion(torneo));
+      } else {
+        setIsTorneo(false);
+      }
+    }
+    return () => {};
+  }, [torneo]);
+
+  if (isTorneo) {
     return (
       <div className="CP-EditarGaleria">
         <NuevoTorneo datosParaEditar={torneo} isEditarTorneoProps={true}></NuevoTorneo>
