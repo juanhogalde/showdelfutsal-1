416bb343bca68008107ec7920e466cd9d4991605
LeoLowa <leo@lowa.com.ar>
2022-01-31 09:52:19 -0300

    falla publicidades

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,4 +3,4 @@ export const api = '194.113.73.194';
 /* export const api = '192.168.1.39'; */ /*(MAXI)*/
 
 //*** Info Deploy */
-console.log('Version Front v0.0.2');
+console.log('Version Front v0.0.13');
