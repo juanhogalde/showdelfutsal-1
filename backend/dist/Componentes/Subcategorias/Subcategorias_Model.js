dc5e143ea6df9b66931214bea15c088c55502ae6
GabiLowa <gabriel@lowa.com.ar>
2022-02-03 09:27:58 -0300

    eliminando carpeta dist

--- backend/dist/Componentes/Subcategorias/Subcategorias_Model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const SubcategoriasSchema = new mongoose_1.Schema({
-    nombreSubcategoria: { type: String },
-    keySubcategoria: { type: Number },
-    idCategoria: { type: mongoose_1.Schema.Types.ObjectId, ref: 'modeloCategorias', required: true },
-});
-exports.default = (0, mongoose_1.model)('modeloSubcategorias', SubcategoriasSchema);
