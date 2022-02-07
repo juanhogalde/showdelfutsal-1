c04f816b829183d6f244b7a4483509c02dbce201
MaxSantana <maxsantana92ok@gmail.com>
2022-02-07 08:39:52 -0300

    Agregar equipos

--- frontend/src/ComponentesAdmin/AgregarEquipos/AgregarEquipos.js
+++ frontend/src/ComponentesAdmin/AgregarEquipos/AgregarEquipos.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BsPlusCircle} from 'react-icons/bs';
+import {useDispatch} from 'react-redux';
+import {listarEquipos_accion} from '../../Redux/Equipos/AccionesEquipos';
 import BotonLowa from '../BotonLowa/BotonLowa';
 import Selector from '../Selector/Selector';
 import TarjetaEquipo from '../TarjetaEquipo/TarjetaEquipo';
 import './AgregarEquipos.css';
 
 const AgregarEquipos = () => {
+  const dispatch = useDispatch();
   const agregarEquipoZona = () => {
     console.log('func para agregar equipos a zona');
   };
+  useEffect(() => {
+    dispatch(listarEquipos_accion());
+    return () => {};
+  }, [dispatch]);
 
   return (
     <div className="CP-AgregarEquipos">
--- frontend/src/Redux/Equipos/AccionesEquipos.js
+++ frontend/src/Redux/Equipos/AccionesEquipos.js
@@ -1,1 +1,19 @@
 import API from './../Configuracion/api';
+
+export const listarEquipos_accion = () => {
+  return dispatch => {
+    /* dispatch(cargandoListarImagenes_accion()); */
+    API({
+      url: '/equipos/listar',
+      method: 'get',
+    })
+      .then(res => {
+        console.log({res});
+        /* dispatch(listarImagenesExito_accion(res.data.value)); */
+      })
+      .catch(error => {
+        console.log({error});
+        /* dispatch(listarImagenesError_accion()); */
+      });
+  };
+};
