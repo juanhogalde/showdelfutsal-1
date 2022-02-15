b9e9d0473b7ffbedd0b7a9e3cac3be197e0c5dc5
MaxSantana <maxsantana92ok@gmail.com>
2022-02-15 15:33:47 -0300

    storeEstadios

---
+++ frontend/src/Redux/Estadios/ReducerEstadios.js
@@ -0,0 +1,11 @@
+const estadiosPorDefecto = {
+  estadios: [],
+};
+
+const storeEstadios = (state = estadiosPorDefecto, accion) => {
+  switch (accion.type) {
+    default:
+      return state;
+  }
+};
+export default storeEstadios;
