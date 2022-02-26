9136425da5936c349a8b28e684e777d1c1a1b68f
LeoLowa <leo@lowa.com.ar>
2022-02-26 10:36:07 -0300

    Desarrollo

--- frontend/src/Entorno.js
+++ frontend/src/Entorno.js
@@ -1,21 +1,24 @@
 //*** Info Deploy */
 let _version = 'Version Front v0.0.22 - 23-02-22';
-let Enviroment = 'Testing';
+let Enviroment = 'Desarrollo';
 let _dominio = 'localhost';
 let _api = 'localhost';
 let _puerto = '4000';
 let _puertoFront = '3000';
+let _urlEscudos = `http://${_dominio}:${_puertoFront}/escudos/`;
 switch (Enviroment) {
   case 'Produccion':
     _dominio = 'http://testingfutsal.c1470936.ferozo.com';
     _api = '194.113.73.194';
     _puerto = '4200';
+    _urlEscudos = `${_dominio}/escudos/`;
     break;
 
   case 'Testing':
     _dominio = 'http://testingfutsal.lowa.com.ar';
     _api = '194.113.73.194';
     _puerto = '4000';
+    _urlEscudos = `${_dominio}/escudos/`;
     console.log(`${Enviroment} :  ${_version}`);
     break;
 
@@ -32,4 +35,4 @@ export const api = _api;
 export const dominio = _dominio;
 export const puerto = _puerto;
 export const server = `http://${_api}:${_puerto}`; //urlImagenes
-export const urlEscudos = `http://${_dominio}:${_puertoFront}/escudos/`; //staticImagenes
+export const urlEscudos = _urlEscudos;
