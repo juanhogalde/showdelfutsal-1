8793d00b47532d0d66d5943b60948af487f4ca9b
GabiLowa <gabriel@lowa.com.ar>
2022-03-28 14:20:11 -0300

    Cambiando entorno a produccion

--- frontend/src/Entorno.js
+++ frontend/src/Entorno.js
@@ -1,6 +1,6 @@
 //*** Info Deploy */
 let _version = 'Version Front v0.0.32 - 26-03-22';
-let Enviroment = 'Produccion';
+let Enviroment = 'Desarrollo';
 let _dominio = 'localhost';
 let _api = 'localhost';
 let _puerto = '4000';
