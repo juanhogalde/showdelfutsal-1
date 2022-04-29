15561562feda4e2833f411451c2641b0ca5155e0
GabiLowa <gabriel@lowa.com.ar>
2022-04-29 13:08:00 -0300

    Corrigiendo Retorno titulo SuperDotado

--- frontend/src/ComponentesAdmin/BarraDeNavegacionAdmin/RetornaTituloDeNavegacion.js/RetornaTituloDeNavegacion.js
+++ frontend/src/ComponentesAdmin/BarraDeNavegacionAdmin/RetornaTituloDeNavegacion.js/RetornaTituloDeNavegacion.js
@@ -5,16 +5,17 @@ const RetornaTituloDeNavegacion = () => {
   const locacion = useLocation();
 
   if (locacion.pathname !== '/') {
-    if (locacion.pathname.split('/').length !== 2) {
-      return <h4>{`${locacion.pathname.split('/')[2]} ${locacion.pathname.split('/')[1]}`}</h4>;
-      /* if (locacion.pathname.split('/').length === 4) {
-        return <h4>{`${locacion.pathname.split('/')[2]} ${locacion.pathname.split('/')[1]}`}</h4>;
-      } else {
-        return <h4>{`${locacion.pathname.split('/')[2]} ${locacion.pathname.split('/')[1]}`}</h4>;
-      } */
-    } else {
-      return <h4>{locacion.pathname.split('/')}</h4>;
-    }
+    return <h4>{locacion.pathname.split('/')[1]}</h4>;
+    // if (locacion.pathname.split('/').length !== 2) {
+    //   return <h4>{`${locacion.pathname.split('/')[2]} ${locacion.pathname.split('/')[1]}`}</h4>;
+    //   /* if (locacion.pathname.split('/').length === 4) {
+    //     return <h4>{`${locacion.pathname.split('/')[2]} ${locacion.pathname.split('/')[1]}`}</h4>;
+    //   } else {
+    //     return <h4>{`${locacion.pathname.split('/')[2]} ${locacion.pathname.split('/')[1]}`}</h4>;
+    //   } */
+    // } else {
+    //   return <h4>{locacion.pathname.split('/')}</h4>;
+    // }
   } else {
     return <h4>El show del futsal</h4>;
   }
