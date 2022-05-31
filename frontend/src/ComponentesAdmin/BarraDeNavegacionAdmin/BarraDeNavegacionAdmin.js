4a2584fa23213007bfc91cf79381aeb6017fd861
GabiLowa <gabriel@lowa.com.ar>
2022-05-31 14:51:47 -0300

    quitar boton menu hamburguesa

--- frontend/src/ComponentesAdmin/BarraDeNavegacionAdmin/BarraDeNavegacionAdmin.js
+++ frontend/src/ComponentesAdmin/BarraDeNavegacionAdmin/BarraDeNavegacionAdmin.js
@@ -1,79 +1,27 @@
 import React from 'react';
 import './BarraDeNavegacionAdmin.css';
-import iconoMenu from '../../Static/Admin/menuAdmin.svg';
 import {useHistory, useLocation} from 'react-router';
 import {BsPower} from 'react-icons/bs';
 import iconoAtras from '../../Static/Admin/iconoAtras.svg';
 import RetornaTituloDeNavegacion from './RetornaTituloDeNavegacion.js/RetornaTituloDeNavegacion';
-// import {
-//   estadoComponenteAgregarEquipo_accion,
-//   ultimaUbicacionEditarTorneo_accion,
-//   volverPorDefectoUnTorneo_accion,
-// } from '../../Redux/Torneos/AccionesTorneos';
-// import {useDispatch} from 'react-redux';
 
-const BarraDeNavegacionAdmin = ({
-  abrirMenuLateral = () => {
-    console.log('No se envió función');
-  },
-}) => {
+const BarraDeNavegacionAdmin = () => {
   const cerrarSesion = () => {
     localStorage.removeItem('token');
     window.location.href = '/Administrador';
   };
-
   const locacion = useLocation();
   const historialDeNavegacion = useHistory();
-  // const dispatch = useDispatch();
-  // const volverAtras = () => {
-  //   if (locacion.pathname.split('/').length !== 2) {
-  //     if (locacion.pathname.split('/')[1] === 'Noticia') {
-  //       historialDeNavegacion.push('/Noticias');
-  //     }
-  //     if (locacion.pathname.split('/')[1] === 'Galería') {
-  //       historialDeNavegacion.push('/Galerías');
-  //     }
-  //     if (locacion.pathname.split('/')[1] === 'Publicidad') {
-  //       historialDeNavegacion.push('/Publicidad');
-  //     }
-  //     if (locacion.pathname.split('/')[1] === 'Torneo') {
-  //       if (locacion.pathname.split('/').length === 4) {
-  //         dispatch(ultimaUbicacionEditarTorneo_accion(true));
-  //         historialDeNavegacion.goBack();
-  //       } else {
-  //         if (locacion.pathname.split('/').length === 7) {
-  //           dispatch(estadoComponenteAgregarEquipo_accion(true));
-  //         } else {
-  //           historialDeNavegacion.goBack();
-  //         }
-  //       }
-  //     }
-  //     if (locacion.pathname.split('/')[1] === 'Vivo') {
-  //       historialDeNavegacion.goBack();
-  //     }
-
-  //     if (locacion.pathname.split('/')[1] === 'Enfrentamientos') {
-  //       if (locacion.pathname.split('/')[2] === 'Editor') {
-  //         dispatch(volverPorDefectoUnTorneo_accion());
-  //         historialDeNavegacion.goBack();
-  //       }
-  //     }
-  //   } else {
-  //     historialDeNavegacion.push('/');
-  //   }
-  // };
-
   return (
     <div className="CP-BarraDeNavegacionAdmin">
       <div
         className="CI-IconoMenu"
+        style={{visibility:locacion.pathname === "/"? 'collapse':'visible'}}
         onClick={
-          locacion.pathname !== '/'
-            ? () => historialDeNavegacion.goBack()
-            : () => abrirMenuLateral()
+           () => historialDeNavegacion.goBack()
         }
       >
-        <img alt="" src={`${locacion.pathname !== '/' ? iconoAtras : iconoMenu}`}></img>
+        <img alt="" src={iconoAtras}></img>
       </div>
       <div className="CI-BarraDeNavegacion">
         <RetornaTituloDeNavegacion></RetornaTituloDeNavegacion>
--- frontend/src/ComponentesAdmin/HomePrivada/HomePrivada.js
+++ frontend/src/ComponentesAdmin/HomePrivada/HomePrivada.js
@@ -1,35 +1,18 @@
 import React from 'react';
 import BarraDeNavegacionAdmin from '../BarraDeNavegacionAdmin/BarraDeNavegacionAdmin';
-import NavegacionLateral from '../NavegacionLateral/NavegacionLateral';
 import './HomePrivada.css';
 
-import {useState} from 'react';
 import {useSelector} from 'react-redux';
 
 import RutasPrivadas from '../../Rutas/RutasPrivadas';
 
 const HomePrivada = () => {
-  const [isMenuLateralAbierto, setIsAperturaLateral] = useState(false);
   const {usuarioLogueado} = useSelector(state => state.storeLogueo);
-
-  const abrirMenuLateral = () => {
-    console.log('Se ejecuto funcion');
-
-    if (isMenuLateralAbierto) {
-      setIsAperturaLateral(false);
-    } else {
-      setIsAperturaLateral(true);
-    }
-  };
   return (
     <React.Fragment>
       {usuarioLogueado && !usuarioLogueado.isRecuperarContraseña && (
-        <BarraDeNavegacionAdmin abrirMenuLateral={abrirMenuLateral}></BarraDeNavegacionAdmin>
+        <BarraDeNavegacionAdmin></BarraDeNavegacionAdmin>
       )}
-      <NavegacionLateral
-        isMenuLateralAbierto={isMenuLateralAbierto}
-        abrirMenuLateral={abrirMenuLateral}
-      />
       <RutasPrivadas></RutasPrivadas>
     </React.Fragment>
   );
