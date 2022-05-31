6a1f275a75037a1922e9f7ce30e0daea68dc4cc2
GabiLowa <gabriel@lowa.com.ar>
2022-05-31 13:09:33 -0300

    organizando rutas

--- frontend/src/Rutas/RutasPrivadas.js
+++ frontend/src/Rutas/RutasPrivadas.js
@@ -43,39 +43,33 @@ const RutasPrivadas = () => {
     return (
       <React.Fragment>
         <Switch>
-          <Route exact component={InicioAdmin} path="/"></Route>
-
+          {/*RUTAS INICIALES */}
+          <Route exact component={PaginaTablaAdmin} path="/Tablas"></Route>
           <Route exact component={PaginaPublicidadAdmin} path="/Publicidad"></Route>
+          <Route exact component={PaginaNoticiasAdmin} path="/Noticias"></Route>
+          <Route exact component={PaginaGaleriaAdmin} path="/Galerías"></Route>
+          <Route exact component={PaginaTorneosAdmin} path="/Torneos"></Route>
+          <Route exact component={PaginaVivoAdmin} path="/Vivo"></Route>
+          <Route exact component={NuevoEnfrentamiento} path="/Enfrentamientos/nuevo"></Route>
+           {/*FIN RUTAS INICIALES */}
+          <Route exact component={InicioAdmin} path="/"></Route>
           <Route exact component={EditarPublicidad} path="/Publicidad/Editar/:id"></Route>
           <Route exact component={NuevaPublicidad} path="/Publicidad/Nueva"></Route>
-
-          <Route exact component={PaginaTorneosAdmin} path="/Torneos"></Route>
           <Route exact component={NuevoTorneo} path="/Torneo/Nuevo"></Route>
           <Route exact component={EditarTorneo} path="/Torneo/Editar/:id"></Route>
-
           <Route exact component={Campeonato} path="/Torneo/Nuevo/Campeonato"></Route>
           <Route exact component={Campeonato} path="/Torneo/Editar/Campeonato/:id"></Route>
-
-          <Route exact component={NuevoEnfrentamiento} path="/Enfrentamientos/nuevo"></Route>
           <Route exact component={CargarEnfrentamiento} path="/Enfrentamientos/:zonaId"></Route>
-
           {/* <Route exact component={EditorEnfrentamientos} path="/Enfrentamientos/Editor"></Route> */}
-
           <Route exact component={AgregarEquipos} path="/Equipos/:zonaId"></Route>
-          <Route exact component={PaginaTablaAdmin} path="/Tablas"></Route>
-
-          <Route exact component={PaginaGaleriaAdmin} path="/Galerías"></Route>
           <Route exact component={NuevaGaleria} path="/Galería/Nueva"></Route>
           <Route exact component={NuevaGaleriaVideo} path="/Galería/Video/:tipo"></Route>
           <Route exact component={EditarGaleria} path="/Galería/Editar/:id"></Route>
-          <Route exact component={PaginaNoticiasAdmin} path="/Noticias"></Route>
           <Route exact component={NuevaNoticia} path="/Noticia/Nueva"></Route>
           <Route exact component={VistaPreviaNoticia} path="/Noticia/Ver"></Route>
           <Route exact component={EditarNoticia} path="/Noticia/Editar"></Route>
-          <Route exact component={PaginaVivoAdmin} path="/Vivo"></Route>
           <Route exact component={NuevoVivo} path="/Vivo/Nuevo/:tipo"></Route>
           <Route exact component={NuevoVivo} path="/Vivo/Editar/:tipo"></Route>
-
           <Route exact path="/fixture">
             <h1>Fixture</h1>
           </Route>
